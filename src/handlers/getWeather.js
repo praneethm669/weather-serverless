4c6712880c00e550152b17e7e1149a3b468bd3d8
Praneeth <praneethm669@gmail.com>
2021-03-08 19:02:36 +0530

    Integrate 3rd party API

--- src/handlers/getWeather.js
+++ src/handlers/getWeather.js
@@ -1,7 +1,24 @@
+const got = require('got');
+
 async function getWeather(event, context) {
+
+  console.log(event.queryStringParameters.zip);
+
+  const zip = event.queryStringParameters.zip;
+
+  const response = await got(`https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=b9d8502288bbe640ecf8a5c8c0d96616&units=imperial`, {
+    https: {
+      rejectUnauthorized: true
+    }
+  });
+  console.log(response);
+  const parsedRes = JSON.parse(response.body);
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Welcome to weather app' }),
+    body: JSON.stringify({
+      Temp: `Current Temperature is : ${(parsedRes.main.temp)}`,
+      Zipcode: `Zipcode is ${zip}`
+    })
   };
 }
 
